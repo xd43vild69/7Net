bf4b091ff1e3d1248f4ea5c5a58a558b407acb59
dgomez <xd43vild69@gmail.com>
2023-04-17 07:27:04 -0500

    Add blazor MAUI to reuse web technologies.

---
+++ 7Net/7NetMauiBlazor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _7NetMauiBlazor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ 7Net/7NetMauiBlazor/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _7NetMauiBlazor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
